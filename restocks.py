32a21f54c4da8d0224b0b74c298c4a3409837e67
nudam00 <dratwap@gmail.com>
2022-12-13 14:34:39 +0100

    outdated

--- restocks.py
+++ restocks.py
@@ -5,6 +5,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 
+# OUTDATED
+
 
 class Restocks:
 
